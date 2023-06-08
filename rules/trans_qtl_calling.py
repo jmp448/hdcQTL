fba9786f6f29ac1767dcc075a49eb3555114c11e
jpopp <jpopp@login02.rockfish.cluster>
2023-06-08 15:10:11 -0400

    udpate trans eqtl calling through variant selection

--- rules/trans_qtl_calling.py
+++ rules/trans_qtl_calling.py
@@ -1,13 +1,13 @@
 rule list_samples_gtex_tissue:
     input:
-        "~/scratch16-abattle4/lab_data/GTEx_v8/sample_annotations/GTEx_Analysis_2017-06-05_v8_Annotations_SampleAttributesDS.txt"
+        "/home/jpopp/scratch16-abattle4/lab_data/GTEx_v8/sample_annotations/GTEx_Analysis_2017-06-05_v8_Annotations_SampleAttributesDS.txt"
     output:
-        "~/data-abattle4/jpopp/GTEx_misc/{tissue}_donors.txt"
+        "data/trans_qtl_calling/gtex/{tissue}_donors.txt"
     params:
         tissue_string="Nerve - Tibial"
     shell:
         """
-        cut -f1,14 {input} | grep {params.tissue_string} | cut -d: -f2 | cut -d- -f1,2 | sort -u | tail -n +2 > {output}
+        cut -f1,14 {input} | grep '{params.tissue_string}' | cut -d: -f2 | cut -d- -f1,2 | sort -u | tail -n +2 > {output}
         """
         # cut -f1,14 {input} | grep Nerve - Tibial | cut -d: -f2 | cut -d- -f1,2 | sort -u | tail -n +2 > /scratch16/abattle4/jpopp/GTEx_misc/{tissue}_donors.txt
   
@@ -16,12 +16,12 @@ rule plink_genotype_reformat_trans:
     resources:
         mem_mb=100000
     input:
-        genotypes="~/scratch16-abattle4/lab_data/GTEx_v8/genotypes/WGS/variant_calls/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.vcf.gz",
-        inds="~/data-abattle4/jpopp/GTEx_misc/{tissue}_donors.txt",
-        trans_candidates="results/static_eqtl_followup/eb_cellid/pseudobulk_tmm/basic/{npcs}/trans_eqtl_variant_candidates.{gs}.txt"
+        genotypes="/home/jpopp/scratch16-abattle4/lab_data/GTEx_v8/genotypes/WGS/variant_calls/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.vcf.gz",
+        inds="data/trans_qtl_calling/gtex/{tissue}_donors.txt",
+        trans_candidates="results/static_eqtl_followup/eb_cellid/pseudobulk_tmm/basic/8pcs/trans_eqtl_variant_candidates.{gs}.txt"
     output:
-        expand("data/dynamic_qtl_calling/{{trajectory}}_{{nbins}}/pseudobulk_tmm/genotypes_filtered_plink.{out}", out=['bed', 'bim', 'fam'])
+        expand("data/trans_qtl_calling/gtex/genotypes_filtered_plink.{{tissue}}.{{gs}}.{out}", out=['bed', 'bim', 'fam'])
     params:
-        prefix="data/dynamic_qtl_calling/{trajectory}_{nbins}/pseudobulk_tmm/genotypes_filtered_plink"
+        prefix="data/trans_qtl_calling/gtex/genotypes_filtered_plink"
     shell:
-        "code/static_eqtl_followup/plink_genotype_reformat_trans.sh {input.genotypes} {input.inds} {input.trans_candidates} {params.prefix}"
+        "code/static_eqtl_followup/plink_genotype_reformat_trans.sh {input.genotypes} {input.inds} {input.trans_candidates} {params.prefix}.{wildcards.tissue}.{wildcards.gs}"
